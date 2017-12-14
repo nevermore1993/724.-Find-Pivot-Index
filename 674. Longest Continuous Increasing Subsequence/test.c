e3e8831359dc3ed9bb96a80dbc54a22a0c1a8386
nevermore1993 <34294200+nevermore1993@users.noreply.github.com>
2017-12-14 11:47:21 +0800

    Update test.c

--- 674. Longest Continuous Increasing Subsequence/test.c
+++ 674. Longest Continuous Increasing Subsequence/test.c
@@ -18,3 +18,25 @@ int findLengthOfLCIS(int* nums, int numsSize) {
     }
     return max;
 }
+
+version2: j is unnecessary
+int findLengthOfLCIS(int* nums, int numsSize) {
+    int max = 0;
+    int curr;
+    int i = 0;
+    while(i < numsSize) {
+        curr = 1;
+        for(; i < numsSize; i++) {
+            if(i == numsSize - 1)
+                break;
+            else if(nums[i] < nums[i + 1]) {
+                curr++;
+            }
+            else
+                break;
+        }
+        max = max < curr ? curr : max;
+        i++;
+    }
+    return max;
+}
