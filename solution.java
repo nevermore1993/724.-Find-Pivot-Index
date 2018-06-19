6df77a39299a97bb31e9da544d2dd75561d76ea1
nevermore1993 <34294200+nevermore1993@users.noreply.github.com>
2018-06-19 20:44:25 +0800

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) 
+            return l2;
+        else if (l2 == null) 
+            return l1;
+        ListNode dummy = new ListNode(0);
+        ListNode pointer;
+        // pointer1 = l1;
+        // pointer2 = l2;
+        
+        if (l1.val <= l2.val) {
+            dummy.next = l1;
+            l1 = l1.next;
+        } else {
+            dummy.next = l2;
+            l2 = l2.next;
+        }
+        pointer = dummy.next;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                pointer.next = l1;
+                pointer = pointer.next;
+                l1 = l1.next;
+            } else {
+                pointer.next = l2;
+                pointer = pointer.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) {
+            while (l2 != null) {
+                pointer.next = l2;
+                pointer = pointer.next;
+                l2 = l2.next;
+            }
+        } else {
+            while (l1 != null) {
+                pointer.next = l1;
+                pointer = pointer.next;
+                l1 = l1.next;
+            }
+        }
+        
+        return dummy.next;
+    }
+}
