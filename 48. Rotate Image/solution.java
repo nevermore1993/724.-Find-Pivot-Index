e3eaadf0d9f98dee1129708294f0b50f7db83fdb
nevermore1993 <34294200+nevermore1993@users.noreply.github.com>
2018-09-03 18:26:08 +0800

    Create solution.java

---
+++ 48. Rotate Image/solution.java
@@ -0,0 +1,18 @@
+// rotate each circle, watch out for the last element in each row, its already rotated
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix[0].length;
+        if (n == 0)
+            return;
+        
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
