1b605456894053ed1ea67ee2d0fb2ab532a18414
szz <34294200+nevermore1993@users.noreply.github.com>
2018-09-06 16:41:15 +0800

    Create solution.java

---
+++ 70. Climbing Stairs/solution.java
@@ -0,0 +1,15 @@
+// dynamic programming 
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+    
+        int[] result = new int[n + 1];
+        result[1] = 1;
+        result[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2]; 
+        }
+        return result[n];
+    }
+}
