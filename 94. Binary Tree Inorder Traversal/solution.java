4ef1f316f4d9fa91751c92f7753d334314e8ebc2
szz <34294200+nevermore1993@users.noreply.github.com>
2018-09-10 11:59:22 +0800

    Create solution.java

---
+++ 94. Binary Tree Inorder Traversal/solution.java
@@ -0,0 +1,72 @@
+// recursive
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (root != null)
+            inorder(result, root);
+        
+        return result;
+    }
+    
+    public void inorder(List<Integer> result, TreeNode root) {
+        if (root != null) {
+            inorder(result, root.left);
+            result.add(root.val);
+            inorder(result, root.right);
+        }
+    }
+}
+
+// Iterating method using stack
+public class Solution {
+    public List < Integer > inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        Stack < TreeNode > stack = new Stack < > ();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+}
+
+// mprris traversal, O(n) time, O(1) space, use right most null node to store parent node
+class Solution {
+    public List < Integer > inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        TreeNode curr = root;
+        TreeNode pre;
+        while (curr != null) {
+            if (curr.left == null) {
+                res.add(curr.val);
+                curr = curr.right; // move to next right node
+            } else { // has a left subtree
+                pre = curr.left;
+                while (pre.right != null) { // find rightmost
+                    pre = pre.right;
+                }
+                pre.right = curr; // put cur after the pre node
+                TreeNode temp = curr; // store cur node
+                curr = curr.left; // move cur to the top of the new tree
+                temp.left = null; // original cur left be null, avoid infinite loops
+            }
+        }
+        return res;
+    }
+}
