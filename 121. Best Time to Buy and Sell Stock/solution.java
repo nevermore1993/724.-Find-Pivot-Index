ba4266cd2c2a111e6bce6d78889f251211c804df
szz <34294200+nevermore1993@users.noreply.github.com>
2018-09-11 14:59:29 +0800

    Create solution.java

---
+++ 121. Best Time to Buy and Sell Stock/solution.java
@@ -0,0 +1,15 @@
+// one pass, if prices decrease, change min, else change profit. find minIn and maxprofit in one pass, cuz minIn will stay still
+// when it reaches valley
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
