3089c99943b269c282bd52d324c31e48719cae3c
szz <34294200+nevermore1993@users.noreply.github.com>
2018-09-22 11:41:20 +0800

    Update solution.java

--- 127. Word Ladder/solution.java
+++ 127. Word Ladder/solution.java
@@ -48,3 +48,67 @@ class Solution {
             return false;
     }
 }
+
+
+// accepted solution, use set to replace list. set.contains() is way faster than list.contains().
+// when length = 1, begin set store all words than can be reached within one step, when length = 2, it stores all words than
+// can be reached within one step from previous beginset.
+// swap begin set and end set if begin.size() > end.size()  to reduce time complexity
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> words = new HashSet<>();
+        for (String s : wordList)
+            words.add(s);
+        
+        return helper(beginWord, endWord, words);
+    }
+    
+    public int helper(String beginWord, String endWord, Set<String> wordList) {
+        Set<String> beginSet = new HashSet<String>(), endSet = new HashSet<String>();
+
+        if (!wordList.contains(endWord))
+            return 0;
+        int len = 1;
+	    int strLen = beginWord.length();
+	    HashSet<String> visited = new HashSet<String>();
+	
+	    beginSet.add(beginWord);
+	    endSet.add(endWord);
+	
+	    while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+		    if (beginSet.size() > endSet.size()) {
+		    	Set<String> set = beginSet;
+		    	beginSet = endSet;
+		    	endSet = set;
+		    }
+
+		    Set<String> temp = new HashSet<String>();
+		    for (String word : beginSet) {
+			    char[] chs = word.toCharArray();
+
+			    for (int i = 0; i < chs.length; i++) {
+				    for (char c = 'a'; c <= 'z'; c++) {
+					    char old = chs[i];
+					    chs[i] = c;
+					    String target = String.valueOf(chs);
+
+					    if (endSet.contains(target)) {
+						    return len + 1;
+				    	}
+
+					    if (!visited.contains(target) && wordList.contains(target)) {
+						    temp.add(target);
+						    visited.add(target);
+				    	}
+					    chs[i] = old;
+				    }
+			    }
+		    }
+
+		    beginSet = temp;
+		    len++;
+    	}
+	
+	    return 0;
+    }
+}
