d7b274e85c07802a55075cc59b885f060f726d25
szz <34294200+nevermore1993@users.noreply.github.com>
2018-09-26 09:45:09 +0800

    Update solution.java

--- 128. Longest Consecutive Sequence/solution.java
+++ 128. Longest Consecutive Sequence/solution.java
@@ -1,3 +1,8 @@
+//use hashmap to store every element. use containsKey() to find if there is consecutive number of current key. containsKey() has
+//O(1) time complexity. set the value of each nood key to length, value of visited key to -1. every time we encounter a 
+// non negetive value, it impies that we have found a subsequence, just add them together.
+// key point here is to store length of a subsequence as key's value. because we dont do sort here, so we might start at the middle
+// of a sequence.
 class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0)
@@ -12,7 +17,7 @@ class Solution {
         
         for (Map.Entry<Integer, Integer> e : map.entrySet()) {
             if (e.getValue() == -1)
-                break;
+                continue;
             int length = 1;
             int key = e.getKey();
             while (map.containsKey(key + 1)) {
@@ -24,8 +29,29 @@ class Solution {
                 key++;
                 map.put(key, -1);
             }
-            map.put(key, length);
+            map.put(e.getKey(), length);
             max = Math.max(max, length);
+        }
+        return max;
+    }
+}
+
+// sorted version, sort first, scan then.
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        int i=0;
+        int max = 0;
+    
+        while ( i<nums.length){
+            int localmax=1;
+
+            while( i+1<nums.length && nums[i+1]-nums[i]==1 || i+1<nums.length && nums[i+1]== nums[i]){
+                if(nums[i+1] != nums[i]) localmax++; 
+                i++;
+            }
+            if (localmax>max) max=localmax;
+            i++;
         }
         return max;
     }
