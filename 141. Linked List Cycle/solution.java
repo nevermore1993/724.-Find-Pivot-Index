ab9af364df407224975bb1388e05b9360e135cb2
szz <34294200+nevermore1993@users.noreply.github.com>
2018-09-29 11:02:59 +0800

    Create solution.java

---
+++ 141. Linked List Cycle/solution.java
@@ -0,0 +1,13 @@
+// two pointers, one moves step by step, the other one moves faster, say two steps a time. if there is a cycle, two pointers will meet at
+// some point
+public boolean hasCycle(ListNode head) {
+    if(head==null) return false;
+    ListNode walker = head;
+    ListNode runner = head;
+    while(runner.next!=null && runner.next.next!=null) {
+        walker = walker.next;
+        runner = runner.next.next;
+        if(walker==runner) return true;
+    }
+    return false;
+}
