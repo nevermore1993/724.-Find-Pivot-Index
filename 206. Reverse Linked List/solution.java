e27feda2d157ac08fd20eb53757cbac385fcbc7b
szz <34294200+nevermore1993@users.noreply.github.com>
2018-10-11 14:09:59 +0800

    Create solution.java

---
+++ 206. Reverse Linked List/solution.java
@@ -0,0 +1,24 @@
+// cur = null， next = head，开始遍历。先保存next.next，然后将next的next指向cur，接着更新next。先记录下下一个结点，和前一个
+// 结点，然后让当前节点指向前一个结点，再更新当前节点为原本的下一个结点，实现逆转
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode next, cur;
+        cur = null;
+        next = head;
+        while (next != null) {
+            ListNode temp = next.next;
+            next.next = cur;
+            cur = next;
+            next = temp;
+        }
+        return cur;
+    }
+}
