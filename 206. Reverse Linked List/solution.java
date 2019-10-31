5e1dedb64959ffeae9dd014cd068852c6f0d4fc9
szz <34294200+nevermore1993@users.noreply.github.com>
2019-10-31 23:05:47 +0800

    Update solution.java

--- 206. Reverse Linked List/solution.java
+++ 206. Reverse Linked List/solution.java
@@ -24,3 +24,14 @@ class Solution {
 }
 
 // 也可以用stack，先顺序入栈，再出栈，则得到逆序链表
+
+
+// 递归实现。递归实现的难点在于递归只能返回子串的头元素，我们需要的是将已经逆序好的子串的尾指向当前元素。实际上当前元素的next指向的就是
+// 已经逆序好的子串的尾，我们可以通过这个关系实现子串和当前元素的连接。
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+}
