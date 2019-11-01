64ccae77d6f0f61a3564842965b0f4f12fdbc59b
szz <34294200+nevermore1993@users.noreply.github.com>
2019-11-01 21:13:59 +0800

    Update solution.java

--- 78. Subsets/solution.java
+++ 78. Subsets/solution.java
@@ -22,3 +22,25 @@ class Solution {
         }       
     }
 }
+
+
+// 递归，与排列不同的是，每次添加新元素之后都能将temp加入到结果中。而且遍历过的元素不再需要了，所以每次递归时要将已经遍历过的元素去掉。
+// 即System.arraycopy(nums, i+1, remain, 0, nums.length - i - 1);  如果不去掉，就会出现重复的组合。
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res, new ArrayList<Integer>(), nums);
+        return res;
+    }
+    public void helper(List<List<Integer>> res, List<Integer> temp, int[] nums) {
+        res.add(new ArrayList<>(temp));
+        
+        for (int i = 0; i < nums.length; i++) {
+            temp.add(nums[i]);
+            int[] remain = new int[nums.length - i - 1];
+            System.arraycopy(nums, i+1, remain, 0, nums.length - i - 1);
+            helper(res, temp, remain);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
